d8d3d16a1963a4a37c0c5fa21fe7812345027ec9
k-xo <alkassimk@gmail.com>
2023-02-08 09:45:37 +0100

    add gas comparisons

---
+++ test/integration/IntegrationTestGasComparisons.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {IntegrationTestHelper, IERC20, console, Balances} from "test/integration/IntegrationTestHelper.sol";
+import {IUniswapV2Router, IUniswapV3Router, IUniswapV2Factory} from "test/integration/interfaces/IUniswap.sol";
+import {ConstantProduct2} from "test/TestHelper.sol";
+
+/// @dev Tests gas usage of similar functions across Uniswap & Wells
+contract IntegrationTestGasComparisons is IntegrationTestHelper {
+    uint mainnetFork;
+
+    ConstantProduct2 cp;
+    bytes constant data = "";
+
+    IUniswapV2Router uniV2Router = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    IUniswapV2Factory uniV2Factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+    IUniswapV3Router constant uniV3Router = IUniswapV3Router(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+    IWETH constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 constant DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+    IERC20[] _tokens = [DAI, IERC20(WETH)];
+
+    function setUp() public {
+        cp = new ConstantProduct2();
+
+        mainnetFork = vm.createSelectFork("mainnet");
+        assertEq(vm.activeFork(), mainnetFork);
+        vm.rollFork(16_582_192);
+
+        setupWell(_tokens);
+    }
+
+    function testFuzz_wellsWethDaiSwap(uint amountOut) public {
+        uint maxAmountIn = 1000 * 1e18;
+        amountOut = bound(amountOut, 1e18, 500 * 1e18);
+
+        well.swapFrom(_tokens[1], _tokens[0], maxAmountIn, amountOut, user);
+    }
+
+    function testFuzz_wellsWethDaiAddLiquidity(uint amountA, uint amountB) public {
+        vm.pauseGasMetering();
+        uint[] memory amounts = new uint[](2);
+        amounts[0] = bound(amountA, 1e18, 1000e18);
+        amounts[1] = bound(amountB, 1e18, 1000e18);
+        vm.resumeGasMetering();
+
+        well.addLiquidity(amounts, 0, user);
+    }
+
+    function testFuzz_wellsWethDaiRemoveLiquidity(uint amount) public {
+        vm.pauseGasMetering();
+        uint[] memory amounts = new uint[](2);
+        amounts[0] = bound(amount, 1e18, 1000e18);
+        amounts[1] = amounts[0];
+
+        uint[] memory reserves = new uint[](2);
+        reserves[0] = _tokens[0].balanceOf(address(well)) - amounts[0];
+        reserves[1] = _tokens[1].balanceOf(address(well)) - amounts[1];
+
+        uint newLpTokenSupply = cp.calcLpTokenSupply(reserves, data);
+        uint lpAmountBurned = well.totalSupply() - newLpTokenSupply;
+        uint[] memory minAmountsOut = new uint[](2);
+        vm.resumeGasMetering();
+
+        well.removeLiquidity(lpAmountBurned, minAmountsOut, user);
+    }
+
+    function testFuzz_uniswapV2WethDaiAddLiquidity(uint amount) public {
+        amount = bound(amount, 1e18, 1000 * 1e18);
+        _uniSetupHelper(amount, address(uniV2Router));
+
+        uniV2Router.addLiquidity(address(WETH), address(DAI), amount, amount, 1, 1, address(this), block.timestamp);
+    }
+
+    function test_uniswapV2WethDaiRemoveLiquidity(uint amount) public {
+        amount = bound(amount, 1e18, 1000 * 1e18);
+        _uniSetupHelper(amount, address(uniV2Router));
+
+        vm.pauseGasMetering();
+        uniV2Router.addLiquidity(address(WETH), address(DAI), amount, amount, 1, 1, address(this), block.timestamp);
+
+        address pair = uniV2Factory.getPair(address(WETH), address(DAI));
+        uint liquidity = IERC20(pair).balanceOf(address(this));
+        IERC20(pair).approve(address(uniV2Router), type(uint).max);
+
+        vm.resumeGasMetering();
+
+        uniV2Router.removeLiquidity(address(WETH), address(DAI), liquidity, 1, 1, address(this), block.timestamp);
+    }
+
+    function testFuzz_uniswapV2WethDaiSwap(uint amount) public {
+        vm.assume(amount > 0);
+        amount = bound(amount, 1e18, 1000 * 1e18);
+        _uniSetupHelper(amount, address(uniV2Router));
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = address(WETH);
+        path[1] = address(DAI);
+
+        uniV2Router.swapExactTokensForTokens(amount, 0, path, msg.sender, block.timestamp);
+    }
+
+    function testFuzz_uniswapV3WethDaiSwap(uint amount) public {
+        amount = bound(amount, 1e18, 1000 * 1e18);
+        _uniSetupHelper(amount, address(uniV3Router));
+
+        IUniswapV3Router.ExactInputSingleParams memory params = IUniswapV3Router.ExactInputSingleParams({
+            tokenIn: address(WETH),
+            tokenOut: address(DAI),
+            fee: 3000,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amount,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+
+        uniV3Router.exactInputSingle(params);
+    }
+
+    function _uniSetupHelper(uint amount, address router) private {
+        vm.pauseGasMetering();
+        deal(address(WETH), address(this), amount * 2);
+        deal(address(DAI), address(this), amount * 2);
+        WETH.approve(router, type(uint).max);
+        DAI.approve(router, type(uint).max);
+        vm.resumeGasMetering();
+    }
+}
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint amount) external;
+}
---
+++ test/integration/IntegrationTestHelper.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Test, console, stdError} from "forge-std/Test.sol";
+import {Users} from "test/helpers/Users.sol";
+import {TestHelper, Balances} from "test/TestHelper.sol";
+
+import {Well, Call, IERC20} from "src/Well.sol";
+import {Auger} from "src/Auger.sol";
+import {Aquifer} from "src/Aquifer.sol";
+import {ConstantProduct2} from "src/functions/ConstantProduct2.sol";
+import {LibContractInfo} from "src/libraries/LibContractInfo.sol";
+import {IWellFunction} from "src/interfaces/IWellFunction.sol";
+
+abstract contract IntegrationTestHelper is TestHelper {
+    using LibContractInfo for address;
+
+    function setupWell(IERC20[] memory _tokens) internal {
+        Call[] memory _pumps = new Call[](0);
+        setupWell(_tokens, Call(address(new ConstantProduct2()), new bytes(0)), _pumps);
+    }
+
+    function setupWell(IERC20[] memory _tokens, Call memory _function, Call[] memory _pumps) internal {
+        wellFunction = _function;
+        for (uint i = 0; i < _pumps.length; i++) {
+            pumps.push(_pumps[i]);
+        }
+
+        initUser();
+
+        auger = new Auger();
+        Aquifer aquifer = new Aquifer();
+
+        well = Well(aquifer.boreWell(_tokens, wellFunction, pumps, auger));
+
+        // Mint mock tokens to user
+        mintTokens(_tokens, user, initialLiquidity);
+
+        mintTokens(_tokens, user2, initialLiquidity);
+
+        approveMaxTokens(_tokens, user, address(well));
+        approveMaxTokens(_tokens, user2, address(well));
+
+        // Mint mock tokens to TestHelper
+        mintTokens(_tokens, address(this), initialLiquidity * 10);
+        approveMaxTokens(_tokens, address(this), address(well));
+
+        // Add initial liquidity from TestHelper
+        addLiquidityEqualAmount(_tokens, address(this), initialLiquidity);
+    }
+
+    /// @dev mint mock tokens to each recipient
+    function mintTokens(IERC20[] memory _tokens, address recipient, uint amount) internal {
+        for (uint i = 0; i < _tokens.length; i++) {
+            deal(address(_tokens[i]), recipient, amount);
+        }
+    }
+
+    /// @dev approve `spender` to use `owner` tokens
+    function approveMaxTokens(IERC20[] memory _tokens, address owner, address spender) internal prank(owner) {
+        for (uint i = 0; i < _tokens.length; i++) {
+            _tokens[i].approve(spender, type(uint).max);
+        }
+    }
+
+    /// @dev add the same `amount` of liquidity for all underlying tokens
+    function addLiquidityEqualAmount(IERC20[] memory _tokens, address from, uint amount) internal prank(from) {
+        uint[] memory amounts = new uint[](_tokens.length);
+        for (uint i = 0; i < _tokens.length; i++) {
+            amounts[i] = amount;
+        }
+        well.addLiquidity(amounts, 0, from);
+    }
+}
