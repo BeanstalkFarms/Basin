9d36fd869f128dc7a1c68a5962415c2b778d64bb
Silo Chad <silochad@protonmail.com>
2023-02-16 14:59:59 -0600

    fix: test_boreEvent()

--- test/Aquifer.t.sol
+++ test/Aquifer.t.sol
@@ -5,12 +5,20 @@ pragma solidity ^0.8.17;
 import {TestHelper, Well, IERC20, console} from "test/TestHelper.sol";
 import {IWell, Call} from "src/interfaces/IWell.sol";
 import {ConstantProduct} from "src/functions/ConstantProduct.sol";
+import {LibClone} from "src/libraries/LibClone.sol";
 import {Aquifer} from "src/Aquifer.sol";
 
 contract AquiferTest is TestHelper {
     address[] wells;
 
-    event BoreWell(address well, address implementation, IERC20[] tokens, Call wellFunction, Call[] pumps, bytes wellData);
+    event BoreWell(
+        address well,
+        address implementation,
+        IERC20[] tokens,
+        Call wellFunction,
+        Call[] pumps,
+        bytes wellData
+    );
 
     function setUp() public {
         initUser();
@@ -51,16 +59,34 @@ contract AquiferTest is TestHelper {
     // FIXME: should we add a salt when deploying a well so that this can be tested?
     function test_boreEvent() external {
         IERC20[] memory _tokens = getTokens(2);
+        bytes32 salt = bytes32(uint(1));
+
+        address a = LibClone.predictDeterministicAddress(
+            address(wellImplementation),
+            encodeWellImmutableData(address(aquifer), _tokens, wellFunction, pumps),
+            salt,
+            address(aquifer)
+        );
 
         vm.expectEmit(true, true, true, true, address(aquifer));
         emit BoreWell(
-            0x211083f1C8175CBF169B39A1A974aaE3CeDc58B0,
+            a,
             address(wellImplementation),
             _tokens,
             wellFunction,
             pumps,
-            new bytes(0)
+            new bytes(0) // well data
         );
-        well = boreWell(address(aquifer), wellImplementation, getTokens(2), wellFunction, pumps, bytes32(uint(1)));
+
+        well = boreWell(
+            address(aquifer),
+            wellImplementation,
+            _tokens,
+            wellFunction,
+            pumps,
+            salt
+        );
+
+        console.log("Well address", address(well));
     }
 }
