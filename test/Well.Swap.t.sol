f919f49f13e80317a6d1c14d4fb15b1383da16da
Silo Chad <silochad@protonmail.com>
2023-02-17 12:19:16 -0600

    fix(test): expect reversion on swap of identical tokens

--- test/Well.Swap.t.sol
+++ test/Well.Swap.t.sol
@@ -176,21 +176,15 @@ contract WellSwapTest is TestHelper {
     //////////// EDGE CASE: IDENTICAL TOKENS ////////////
 
     /// @dev swapFrom: identical tokens results in no change in balances
-    function testFuzz_swapFrom_sameToken(uint amountIn) public prank(user) check_noTokenBalanceChange {
-        Balances memory userBalance = getBalances(user, well);
-        vm.assume(amountIn > 0);
-        vm.assume(amountIn <= userBalance.tokens[0]);
+    function testFuzz_swapFrom_sameToken(uint amountIn) public prank(user) {
+        vm.expectRevert("Well: Invalid tokens");
         well.swapFrom(tokens[0], tokens[0], amountIn, 0, user);
-        assertEq(well.getSwapOut(tokens[0], tokens[0], amountIn), amountIn, "getSwapOut mismatch");
     }
 
     /// @dev swapTo: identical tokens results in no change in balances
-    function testFuzz_swapTo_sameToken(uint amountOut) public prank(user) check_noTokenBalanceChange {
-        Balances memory userBalance = getBalances(user, well);
-        vm.assume(amountOut > 0);
-        vm.assume(amountOut <= userBalance.tokens[0]);
-        well.swapTo(tokens[0], tokens[0], 100e6, 0, user);
-        assertEq(well.getSwapIn(tokens[0], tokens[0], amountOut), amountOut, "getSwapIn mismatch");
+    function testFuzz_swapTo_sameToken(uint amountOut) public prank(user) {
+        vm.expectRevert("Well: Invalid tokens");
+        well.swapTo(tokens[0], tokens[0], amountOut, 0, user);
     }
 
     modifier check_noTokenBalanceChange() {
