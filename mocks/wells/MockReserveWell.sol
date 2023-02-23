8d14b7b84e58bbf64110cee9decf2dc95bdb0b76
publius <beanstalk.publius@protonmail.com>
2023-02-22 16:53:17 -0800

    Pump Update uinit tests

---
+++ mocks/wells/MockReserveWell.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import {IPump} from "src/interfaces/pumps/IPump.sol";
+
+/**
+ * @title Mock Well that allows setting of reserves.
+ */
+contract MockReserveWell {
+
+    uint[] reserves;
+
+    function setReserves(uint[] memory _reserves) public {
+        reserves = _reserves;
+    }
+
+    function getReserves() external view returns (uint[] memory _reserves) {
+        _reserves = reserves;
+    }
+
+    function update(address pump, uint[] calldata _reserves, bytes calldata data) external {
+        IPump(pump).update(_reserves, data);
+        setReserves(_reserves);
+    }
+}
