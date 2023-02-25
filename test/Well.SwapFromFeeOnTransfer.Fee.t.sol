2bc6fdd1bdbaf28f1067aeb84dff42b90a5df290
Silo Chad <silochad@protonmail.com>
2023-02-24 19:21:44 -0600

    test: add transfer to fee mock, test fee on toToken

--- test/Well.SwapFromFeeOnTransfer.Fee.t.sol
+++ test/Well.SwapFromFeeOnTransfer.Fee.t.sol
@@ -30,25 +30,13 @@ contract WellSwapFromFeeOnTransferFeeTest is TestHelper {
         well.swapFromFeeOnTransfer(tokens[0], tokens[1], amountIn, minAmountOut, user);
     }
 
-    /**
-     * @dev
-     * 
-     * tokens[0]: 1% fee
-     * tokens[1]: No fee
-     * 
-     * Balance changes:
-     *   User: 
-     *      token0: (amountInToWell + fee) -> Well
-     *      token1: (amountOutFromWell) <- Well
-     *   Well:
-     *      token0: (amountInToWell) <- User
-     *      token1: (amountOutFromWell) -> User
-     */
-    function test_swapFromFeeOnTransfer_fee() public prank(user) {
+    ////////// Fee on `fromToken` only
+
+    function test_swapFromFeeOnTransfer_fromToken() public prank(user) {
         uint amountIn0 = 1000 * 1e18;
         _swapFrom_feeOnFromToken(amountIn0);
     }
-    function testFuzz_swapFromFeeOnTransfer_fee(uint amountIn0) public prank(user) {
+    function testFuzz_swapFromFeeOnTransfer_fromToken(uint amountIn0) public prank(user) {
         amountIn0 = bound(amountIn0, 0, tokens[0].balanceOf(address(well)));
         _swapFrom_feeOnFromToken(amountIn0);
     }
@@ -56,13 +44,15 @@ contract WellSwapFromFeeOnTransferFeeTest is TestHelper {
     /**
      * @dev tokens[0]: 1% fee / tokens[1]: No fee
      * 
+     * Swapping from tokens[0] -> tokens[1]
+     * 
      * Resulting balance changes:
      *   User: 
-     *      token0: Transfer (amountIn - fee) to Well
-     *      token1: Receive  (amountOut)      from Well
+     *      token0: Transfer (amountIn) to Well
+     *      token1: Receive (amountOut) from Well
      *   Well:
-     *      token0: Receive  (amountIn - fee) from User
-     *      token1: Transfer (amountOut)      to User
+     *      token0: Receive (amountIn - fee) from User
+     *      token1: Transfer (amountOut) to User
      */
     function _swapFrom_feeOnFromToken(uint amountIn) internal {
         Balances memory userBalanceBefore = getBalances(user, well);
@@ -78,7 +68,7 @@ contract WellSwapFromFeeOnTransferFeeTest is TestHelper {
         // Perform swap
         vm.expectEmit(true, true, true, true);
         emit Swap(tokens[0], tokens[1], amountInWithFee, calcAmountOut);
-        uint amountOut = well.swapFromFeeOnTransfer(tokens[0], tokens[1], amountIn, 0, user);
+        uint amountOut = well.swapFromFeeOnTransfer(tokens[0], tokens[1], amountIn, calcAmountOut, user);
 
         Balances memory userBalanceAfter = getBalances(user, well);
         Balances memory wellBalanceAfter = getBalances(address(well), well);
@@ -86,14 +76,79 @@ contract WellSwapFromFeeOnTransferFeeTest is TestHelper {
         // Since tokens[1] has no fee, our calculation should've been correct
         assertEq(amountOut, calcAmountOut, "actual vs expected output");
 
-        // Compare balances
+        // Fee taken on tokens[0]
         assertEq(userBalanceBefore.tokens[0] - userBalanceAfter.tokens[0], amountIn, "Incorrect token0 user balance");
+        assertEq(wellBalanceAfter.tokens[0], wellBalanceBefore.tokens[0] + amountInWithFee, "Incorrect token0 well reserve");
+
+        // No fee taken on tokens[1]
         assertEq(
             userBalanceAfter.tokens[1] - userBalanceBefore.tokens[1], calcAmountOut, "Incorrect token1 user balance"
         );
-        assertEq(wellBalanceAfter.tokens[0], wellBalanceBefore.tokens[0] + amountInWithFee, "Incorrect token0 well reserve");
         assertEq(
             wellBalanceAfter.tokens[1], wellBalanceBefore.tokens[1] - calcAmountOut, "Incorrect token1 well reserve"
+        );
+    }
+
+    ////////// Fee on `toToken` only
+
+    function test_swapFromFeeOnTransfer_toToken() public prank(user) {
+        uint amountIn0 = 1000 * 1e18;
+        _swapFrom_feeOnToToken(amountIn0);
+    }
+    function testFuzz_swapFromFeeOnTransfer_toToken(uint amountIn0) public prank(user) {
+        amountIn0 = bound(amountIn0, 0, tokens[0].balanceOf(address(well)));
+        _swapFrom_feeOnToToken(amountIn0);
+    }
+
+    /**
+     * @dev tokens[0]: 1% fee / tokens[1]: No fee
+     * 
+     * Swapping from tokens[1] -> tokens[0]
+     * 
+     * Resulting balance changes:
+     *   User: 
+     *      token0: Transfer (amountIn) to Well
+     *      token1: Receive (amountOut - fee) from Well
+     *   Well:
+     *      token0: Receive (amountIn) from User
+     *      token1: Transfer (amountOut) to User
+     * 
+     * NOTE: Since the fee is incurred after `amountOut` is calculated in the Well,
+     * the Swap event contains the amount deducted from the Well, not the amount
+     * given to the User.
+     */
+    function _swapFrom_feeOnToToken(uint amountIn) internal {
+        Balances memory userBalanceBefore = getBalances(user, well);
+        Balances memory wellBalanceBefore = getBalances(address(well), well);
+
+        // Fee on input
+        uint calcAmountOut = well.getSwapOut(tokens[1], tokens[0], amountIn);
+        uint _fee = calcAmountOut * MockTokenFeeOnTransfer(address(tokens[0])).fee() / 1e18;
+
+        // Reduce the amountIn by fee to get expected amount out
+        uint calcAmountOutWithFee = calcAmountOut - _fee;
+        
+        // Perform swap
+        vm.expectEmit(true, true, true, true);
+        emit Swap(tokens[1], tokens[0], amountIn, calcAmountOut);
+        uint amountOut = well.swapFromFeeOnTransfer(tokens[1], tokens[0], amountIn, calcAmountOutWithFee, user);
+
+        Balances memory userBalanceAfter = getBalances(user, well);
+        Balances memory wellBalanceAfter = getBalances(address(well), well);
+        
+        // Fee is applied after `amountOut` is calculated, so this is the amount that the Well sent
+        assertEq(amountOut, calcAmountOut, "amountOut different than calculated");
+
+        // No fee taken on tokens[1]
+        assertEq(userBalanceBefore.tokens[1] - userBalanceAfter.tokens[1], amountIn, "Incorrect token0 user balance");
+        assertEq(wellBalanceAfter.tokens[1], wellBalanceBefore.tokens[1] + amountIn, "Incorrect token0 well reserve");
+
+        // Fee taken on tokens[0]
+        assertEq(
+            userBalanceAfter.tokens[0] - userBalanceBefore.tokens[0], calcAmountOutWithFee, "Incorrect token1 user balance"
+        );
+        assertEq(
+            wellBalanceAfter.tokens[0], wellBalanceBefore.tokens[0] - calcAmountOut, "Incorrect token1 well reserve"
         );
     }
 }
