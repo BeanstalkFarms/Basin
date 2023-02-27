4aa6ef434cd250fe84f77e5b1ebd53708e25a211
Harris Levine <acyclicgraphs@gmail.com>
2023-02-27 15:48:31 -0500

    Add crytic/properties for ABDKMath64x64

---
+++ test/CryticABDKMath64x64Harness.t.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.17;
+import "lib/properties/contracts/Math/ABDKMath64x64/ABDKMath64x64PropertyTests.sol";
+
+contract CryticABDKMath64x64Harness is CryticABDKMath64x64PropertyTests {
+    /* Any additional test can be added here */
+}
