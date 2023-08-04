c30cb10225c4104cf955298cddd9fa00858b19b9
publius <beanstalk.publius@protonmail.com>
2023-08-04 23:36:54 +0200

    1 more grammer fix

--- src/interfaces/IWell.sol
+++ src/interfaces/IWell.sol
@@ -365,8 +365,8 @@ interface IWell {
     //////////////////// RESERVES ////////////////////
 
     /**
-     * @notice Syncs the reserves of the Well with the Well's balances of underlying tokens and if the reserves
-     * increase mints at least `minLpAmountOut` LP Tokens to `recipient`.
+     * @notice Syncs the reserves of the Well with the Well's balances of underlying tokens. If the reserves
+     * increasem, mints at least `minLpAmountOut` LP Tokens to `recipient`.
      * @param recipient The address to receive the LP tokens
      * @param minLpAmountOut The minimum amount of LP tokens to receive
      * @return lpAmountOut The amount of LP tokens received
